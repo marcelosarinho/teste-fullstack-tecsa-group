780702c6d53d42809c8338fe748861bb6feead32
Marcelo Sarinho <marceloactsarinho@gmail.com>
2026-03-18 14:02:21 -0300

    feat: add form tag in create task modal

--- frontend/src/ui/layout.js
+++ frontend/src/ui/layout.js
@@ -16,37 +16,39 @@ export default function renderLayout() {
             <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
           </div>
 
-          <div class="modal-body">
-            <div class="mb-3">
-              <label for="task-name" class="form-label">Nome da tarefa</label>
-              <input type="text" class="form-control" id="task-name" placeholder="Digite o nome da tarefa" />
+          <form>
+            <div class="modal-body">
+              <div class="mb-3">
+                <label for="task-name" class="form-label">Nome da tarefa</label>
+                <input type="text" class="form-control" id="task-name" placeholder="Digite o nome da tarefa" />
+              </div>
+
+              <div class="mb-3">
+                <label for="task-description" class="form-label">Descrição</label>
+                <textarea class="form-control" id="task-description" rows="3" placeholder="Descreva a tarefa"></textarea>
+              </div>
+
+              <div class="mb-3">
+                <label for="task-status" class="form-label">Status</label>
+                <select class="form-select" id="task-status">
+                  ${STATUS.map((st => {
+                    return `<option value="${st.value}">${st.label}</option>`
+                  }))}
+                </select>
+              </div>
             </div>
 
-            <div class="mb-3">
-              <label for="task-description" class="form-label">Descrição</label>
-              <textarea class="form-control" id="task-description" rows="3" placeholder="Descreva a tarefa"></textarea>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-danger" data-bs-dismiss="modal">
+                <i class="bi bi-x"></i>
+                Cancelar
+              </button>
+              <button type="button" class="btn btn-primary">
+                <i class="bi bi-check"></i>
+                Criar
+              </button>
             </div>
-
-            <div class="mb-3">
-              <label for="task-status" class="form-label">Prioridade</label>
-              <select class="form-select" id="task-status">
-                ${STATUS.map((st => {
-                  return `<option value="${st.value}">${st.label}</option>`
-                }))}
-              </select>
-            </div>
-          </div>
-
-          <div class="modal-footer">
-            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">
-              <i class="bi bi-x"></i>
-              Cancelar
-            </button>
-            <button type="button" class="btn btn-primary">
-              <i class="bi bi-check"></i>
-              Criar
-            </button>
-          </div>
+          </form>
         </div>
       </div>
     </div>
