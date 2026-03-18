a7ea19574788b3fc7762584e643e5cd10366c08c
Marcelo Sarinho <marceloactsarinho@gmail.com>
2026-03-18 13:41:39 -0300

    feat: modal to create new task

--- frontend/src/ui/layout.js
+++ frontend/src/ui/layout.js
@@ -5,6 +5,49 @@ export default function renderLayout() {
     <section id="tasks-status"></section>
     <section id="tasks-search"></section>
     <section id="tasks-list"></section>
+
+    <div id="modal-add-task" class="modal fade" tabindex="-1">
+      <div class="modal-dialog modal-sm">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">Adicionar tarefa</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+          </div>
+
+          <div class="modal-body">
+            <div class="mb-3">
+              <label for="task-name" class="form-label">Nome da tarefa</label>
+              <input type="text" class="form-control" id="task-name" placeholder="Digite o nome da tarefa" />
+            </div>
+
+            <div class="mb-3">
+              <label for="task-description" class="form-label">Descrição</label>
+              <textarea class="form-control" id="task-description" rows="3" placeholder="Descreva a tarefa"></textarea>
+            </div>
+
+            <div class="mb-3">
+              <label for="task-priority" class="form-label">Prioridade</label>
+              <select class="form-select" id="task-priority">
+                <option value="low">Baixa</option>
+                <option value="medium">Média</option>
+                <option value="high">Alta</option>
+              </select>
+            </div>
+          </div>
+
+          <div class="modal-footer">
+            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">
+              <i class="bi bi-x"></i>
+              Cancelar
+            </button>
+            <button type="button" class="btn btn-primary">
+              <i class="bi bi-check"></i>
+              Criar
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   </main>
 `
 }
--- frontend/src/ui/tasks-header.js
+++ frontend/src/ui/tasks-header.js
@@ -6,10 +6,19 @@ export default function renderTasksHeader() {
         <h2 class="mb-0">Gerenciamento de Tarefas</h2>
       </div>
 
-      <button class="btn btn-primary d-flex align-items-center gap-1 fs-6">
+      <button class="btn btn-primary d-flex align-items-center gap-1 fs-6" data-bs-toggle="modal" data-bs-target="#modal-add-task">
         <i class="bi bi-plus"></i>
         Adicionar Tarefa
       </button>
     </section>
+
+    <div class="modal fade" id="taskModal" tabindex="-1">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">...</div>
+          <div class="modal-body">...</div>
+        </div>
+      </div>
+    </div>
   `;
 }
