9905830bebb240ae5d2fc5d2080240bf0fd9b5f5
Marcelo Sarinho <marceloactsarinho@gmail.com>
2026-03-18 15:50:12 -0300

    fix: added services and repositories to index.php

--- backend/index.php
+++ backend/index.php
@@ -2,13 +2,15 @@
 
 require_once 'config/database.php';
 require_once 'core/Router.php';
+require_once 'repositories/TaskRepository.php';
+require_once 'services/TaskService.php';
 require_once 'controllers/TaskController.php';
 
 $router = new Router();
 
-$router->add('GET', '/tasks', [TaskController::class, 'index']);
-$router->add('POST', '/tasks', [TaskController::class, 'create']);
-$router->add('PUT', '/tasks/{id}', [TaskController::class, 'update']);
-$router->add('DELETE', '/tasks/{id}', [TaskController::class, 'delete']);
+$router->add('GET', '/tasks', [new TaskController(), 'index']);
+$router->add('POST', '/tasks', [new TaskController(), 'create']);
+$router->add('PUT', '/tasks/{id}', [new TaskController(), 'update']);
+$router->add('DELETE', '/tasks/{id}', [new TaskController(), 'delete']);
 
 $router->run();
