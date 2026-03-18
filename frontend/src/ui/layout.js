447c2fd18621e131711b3920013a2901eb69da05
Marcelo Sarinho <marceloactsarinho@gmail.com>
2026-03-18 13:13:26 -0300

    feat: tasks-header module

--- frontend/src/ui/layout.js
+++ frontend/src/ui/layout.js
@@ -1,21 +1,10 @@
 export function renderLayout() {
   document.querySelector('#app').innerHTML = `
   <main id="todo-list" class="container d-flex flex-column gap-3">
-    <section class="card d-flex flex-column align-items-center gap-3 p-3">
-      <div class="d-flex flex-row align-items-center gap-3">
-        <i class="bi bi-check2-circle fs-1"></i>
-        <h2 class="mb-0">Gerenciamento de Tarefas</h2>
-      </div>
-
-      <button class="btn btn-primary d-flex align-items-center gap-1 fs-6">
-        <i class="bi bi-plus"></i>
-        Adicionar Tarefa
-      </button>
-    </section>
-
-    <section id="tasks-list" class="card d-flex flex-column align-items-center gap-3 p-3">
-      <h3>Lista de Tarefas</h3>
-    </section>
+    <section id="tasks-header"></section>
+    <section id="tasks-search"></section>
+    <section id="tasks-status"></section>
+    <section id="tasks-list"></section>
   </main>
 `
 }
---
+++ frontend/src/ui/tasks-header.js
@@ -0,0 +1,15 @@
+export function renderTasksHeader() {
+  document.querySelector('#app').innerHTML = `
+    <section class="card d-flex flex-column align-items-center gap-3 p-3">
+      <div class="d-flex flex-row align-items-center gap-3">
+        <i class="bi bi-check2-circle fs-1"></i>
+        <h2 class="mb-0">Gerenciamento de Tarefas</h2>
+      </div>
+
+      <button class="btn btn-primary d-flex align-items-center gap-1 fs-6">
+        <i class="bi bi-plus"></i>
+        Adicionar Tarefa
+      </button>
+    </section>
+  `;
+}
