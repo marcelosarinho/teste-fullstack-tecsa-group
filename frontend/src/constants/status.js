64f065e54cf6c5e5d72d8e47e4748bad352f7128
Marcelo Sarinho <marceloactsarinho@gmail.com>
2026-03-18 13:42:02 -0300

    feat: status constants

---
+++ frontend/src/constants/status.js
@@ -0,0 +1,14 @@
+export default {
+    PENDING: {
+      value: 'pendente',
+      label: 'Pendente'
+    },
+    IN_PROGRESS: {
+      value: 'em_andamento',
+      label: 'Em andamento'
+    },
+    COMPLETED: {
+      value: 'concluido',
+      label: 'Concluído'
+    }
+}
