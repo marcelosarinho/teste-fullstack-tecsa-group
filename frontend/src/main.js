b15a15a56429b3c7702a1517af8bc5c8c9214042
Marcelo Sarinho <marceloactsarinho@gmail.com>
2026-03-18 12:39:42 -0300

    feat: tasks search module

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,8 @@
 import './style.css'
 import { renderLayout } from './ui/layout'
 import { renderTasksStatus } from './ui/tasks-status'
+import { renderTasksSearch } from './ui/tasks-search'
 
 renderLayout();
 renderTasksStatus();
+renderTasksSearch();
---
+++ frontend/src/ui/tasks-search.js
@@ -0,0 +1,23 @@
+export function renderTasksSearch() {
+  document.querySelector('#todo-list').innerHTML = `
+    <section id="tasks-search" class="card p-3">
+      <div class="row align-items-center g-3">
+        <div class="col-12 col-md-6">
+          <div class="input-group">
+            <span class="input-group-text">
+              <i class="bi bi-search fs-6"></i>
+            </span>
+            <input type="text" class="form-control" placeholder="Buscar tarefa...">
+          </div>
+        </div>
+
+        <div class="col-12 col-md-6 btn-group gap-3 flex-wrap">
+          <button class="btn btn-primary btn-sm rounded-pill">Todas</button>
+          <button class="btn btn-primary btn-sm rounded-pill">Concluídas</button>
+          <button class="btn btn-primary btn-sm rounded-pill">Pendentes</button>
+          <button class="btn btn-primary btn-sm rounded-pill">Em andamento</button>
+        </div>
+      </div>
+    </section>
+  `
+}
